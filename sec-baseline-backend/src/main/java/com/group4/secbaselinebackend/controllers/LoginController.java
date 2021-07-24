ebf1b17c58b44bd2252ca326d24a264b2675a3da
fffxxxlll <484499213@qq.com>
2021-07-24 20:21:00 +0800

    controller

--- sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/controllers/LoginController.java
+++ sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/controllers/LoginController.java
@@ -1,33 +1,56 @@
 package com.group4.secbaselinebackend.controllers;
 
 import com.group4.secbaselinebackend.models.Admin;
+import com.group4.secbaselinebackend.services.AdminService;
+import com.group4.secbaselinebackend.utils.jwt.JwtHelper;
+import com.group4.secbaselinebackend.valueObjects.ErrorMessage;
+import com.group4.secbaselinebackend.valueObjects.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @author feng xl
- * @date 2021/7/17 0017 19:49
- */
+import javax.servlet.http.HttpServletRequest;
+
+
 
 @Controller
 public class LoginController {
 
 
-    @ResponseBody
-    @GetMapping("/")
-    public String visitHome() {
+    @Autowired
+    AdminService adminService;
 
-        return "home";
-    }
+    @Autowired
+    private JwtHelper jwtHelper;
+    @Value("${token.issuer}")
+    private String issuer;
+    @Value("${token.expired_in}")
+    private int expiredIn;
+
 
     @ResponseBody
     @PostMapping("/login")
-    public String doLogin(@RequestBody Admin admin){
+    public ResponseEntity<?> doLogin(@RequestBody Admin admin) throws Exception {
+        Integer res = adminService.judgeLogin(admin);
+        switch (res){
+            case 0 : return ResponseEntity.ok(new Message(ErrorMessage.ACCOUNT_NOT_EXIST.getMessage()));
+            case 2 : return ResponseEntity.ok(new Message(ErrorMessage.ERROR_LOGIN__NAME_OR_PASSWORD.getMessage()));
+            default: break;
+        }
+        return ResponseEntity.ok(jwtHelper.getToken(issuer, expiredIn, admin));
+    }
 
-
-        return admin.toString();
+    @ResponseBody
+    @PostMapping("/success")
+    public ResponseEntity<?> enterHome(HttpServletRequest request) {
+        if("null".equals(request.getHeader("Jwt-Token")))
+            return ResponseEntity.ok(new Message(ErrorMessage.INVALID_TOKEN.getMessage()));
+        if(!adminService.loginCheck())
+            return ResponseEntity.ok(new Message(ErrorMessage.INVALID_TOKEN.getMessage()));
+        return ResponseEntity.ok(new Message("success"));
     }
 }

5cf8b7ac4a6417a9d97aac575a321d7b5dc9a89d
fffxxxlll <484499213@qq.com>
2021-07-28 10:25:18 +0800

    alert model

---
+++ sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/models/AlertInfo.java
@@ -0,0 +1,24 @@
+package com.group4.secbaselinebackend.models;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.math.BigInteger;
+
+/**
+ * @author feng xl
+ * @date 2021/7/27 0027 16:56
+ */
+
+@Data
+public class AlertInfo {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private BigInteger id;
+    private Integer alertTypeId;
+    private String alertType;
+    private String alertDesc;
+    private Integer level;
+    private Integer isDone;
+}

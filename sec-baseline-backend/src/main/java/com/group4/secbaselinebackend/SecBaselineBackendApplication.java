204c1ba1e3ef25e20de9e7c1fc22c904be9f305c
ebpaidx <1090892502@qq.com>
2021-07-28 14:17:59 +0800

    注解更新（定时）

--- sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/SecBaselineBackendApplication.java
+++ sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/SecBaselineBackendApplication.java
@@ -3,9 +3,13 @@ package com.group4.secbaselinebackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @SpringBootApplication
-@MapperScan("com.group4.secbaselinebackend.mapper")
+@MapperScan("com.group4.secbaselinebackend")
+@EnableScheduling
 public class SecBaselineBackendApplication {
 
     public static void main(String[] args) {

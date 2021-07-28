5479ff4179e3cc487d5939c78602e3ae50676954
fffxxxlll <484499213@qq.com>
2021-07-28 15:55:28 +0800

    mybatis分页插件

---
+++ sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/config/MybatisConfig.java
@@ -0,0 +1,27 @@
+package com.group4.secbaselinebackend.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author feng xl
+ * @date 2021/7/28 0028 15:43
+ */
+
+@Configuration
+@MapperScan("com.group4.secbaselinebackend.mapper.*")
+public class MybatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+
+}

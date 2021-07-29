0a30d004b7b8642417584cf26d3f13d54a1ef0bf
fffxxxlll <484499213@qq.com>
2021-07-29 17:08:01 +0800

    RedisUtil

--- sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/redis/RedisUtil.java
+++ sec-baseline-backend/src/main/java/com/group4/secbaselinebackend/redis/RedisUtil.java
@@ -1,4 +1,4 @@
-package com.group4.secbaselinebackend.utils.redis;
+package com.group4.secbaselinebackend.redis;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
